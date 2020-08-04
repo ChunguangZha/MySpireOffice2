c85ee9d5d7072057757f677ed5853a615c6e23e5
xiaocha0209@hotmail.com <buzhidaoGIT123>
2020-08-05 07:58:09 +0800

    FeisWork

---
+++ FeisWork/FeisWork/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace FeisWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbSalaryCol = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmdEmplyeeNameCol = new System.Windows.Forms.ComboBox();
+            this.btnLookUp = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "员工姓名列：";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(152, 126);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(342, 21);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(30, 126);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(103, 21);
+            this.btnLoadFile.TabIndex = 2;
+            this.btnLoadFile.Text = "导入文件";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbSalaryCol);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmdEmplyeeNameCol);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(496, 90);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "配置";
+            // 
+            // cmbSalaryCol
+            // 
+            this.cmbSalaryCol.FormattingEnabled = true;
+            this.cmbSalaryCol.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z",
+            "AA",
+            "AB",
+            "AC",
+            "AD",
+            "AE",
+            "AF",
+            "AG",
+            "AH",
+            "AI",
+            "AJ",
+            "AK",
+            "AL",
+            "AM",
+            "AN"});
+            this.cmbSalaryCol.Location = new System.Drawing.Point(140, 51);
+            this.cmbSalaryCol.Name = "cmbSalaryCol";
+            this.cmbSalaryCol.Size = new System.Drawing.Size(121, 20);
+            this.cmbSalaryCol.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "导出工资列：";
+            // 
+            // cmdEmplyeeNameCol
+            // 
+            this.cmdEmplyeeNameCol.FormattingEnabled = true;
+            this.cmdEmplyeeNameCol.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z",
+            "AA",
+            "AB",
+            "AC",
+            "AD",
+            "AE",
+            "AF",
+            "AG",
+            "AH",
+            "AI",
+            "AJ",
+            "AK",
+            "AL",
+            "AM",
+            "AN"});
+            this.cmdEmplyeeNameCol.Location = new System.Drawing.Point(140, 19);
+            this.cmdEmplyeeNameCol.Name = "cmdEmplyeeNameCol";
+            this.cmdEmplyeeNameCol.Size = new System.Drawing.Size(121, 20);
+            this.cmdEmplyeeNameCol.TabIndex = 1;
+            // 
+            // btnLookUp
+            // 
+            this.btnLookUp.Location = new System.Drawing.Point(305, 237);
+            this.btnLookUp.Name = "btnLookUp";
+            this.btnLookUp.Size = new System.Drawing.Size(130, 47);
+            this.btnLookUp.TabIndex = 4;
+            this.btnLookUp.Text = "工资汇总";
+            this.btnLookUp.UseVisualStyleBackColor = true;
+            this.btnLookUp.Click += new System.EventHandler(this.btnLookUp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 326);
+            this.Controls.Add(this.btnLookUp);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnLoadFile);
+            this.Controls.Add(this.txtFilePath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "菲姐的工资汇总";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbSalaryCol;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmdEmplyeeNameCol;
+        private System.Windows.Forms.Button btnLookUp;
+    }
+}
+
---
+++ FeisWork/FeisWork/Form1.cs
@@ -0,0 +1,357 @@
+﻿using Spire.Xls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FeisWork
+{
+    public partial class Form1 : Form
+    {
+        private const string GROUP1TITLE = "一、员工银行发放工资";
+        private const string GROUP2TITLE = "二、员工现金发放工资";
+        private const string GROUP3TITLE = "三、外来人员发放工资";
+
+        private string filePath;
+        private List<Employee> listGroup1Employes = new List<Employee>();
+        private List<Employee> listGroup2Employes = new List<Employee>();
+        private List<Employee> listGroup3Employes = new List<Employee>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.cmdEmplyeeNameCol.SelectedIndex = 5;
+            this.cmbSalaryCol.SelectedIndex = 33;
+        }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.Filter = "xlsx文件|*.xlsx";
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.filePath = this.txtFilePath.Text = this.openFileDialog1.FileName;
+                
+            }
+        }
+        
+        private Workbook LoadFile(string filePath)
+        {
+            try
+            {
+                Workbook workbook = new Workbook();
+                workbook.LoadFromFile(filePath);
+
+                for (int i = 0; i < workbook.Worksheets.Count; i++)
+                {
+                    Worksheet sheet = workbook.Worksheets[i];
+                    if (sheet.Name.Trim() == (i + 1).ToString() + "月份")
+                    {
+                        for (int r = sheet.FirstRow; r <= sheet.LastRow; r++)
+                        {
+                            string col1CellValue = sheet[r, 1].Value.Trim();
+                            if (col1CellValue == GROUP1TITLE)
+                            {
+                                r = ReadGroupEmployee(sheet, r + 4, this.listGroup1Employes, i + 1);
+                            }
+                            else if (col1CellValue == GROUP2TITLE)
+                            {
+                                r = ReadGroupEmployee(sheet, r + 1, this.listGroup2Employes, i + 1);
+                            }
+                            else if (col1CellValue == GROUP3TITLE)
+                            {
+                                r = ReadGroupEmployee(sheet, r + 1, this.listGroup3Employes, i + 1);
+                            }
+
+
+
+                            //if (string.IsNullOrEmpty(sheet[r, 1].Value.Trim()))
+                            //{
+                            //    break;
+                            //}
+
+                            Console.WriteLine(sheet[r, 1].Value.Trim());
+                        }
+                    }
+                }
+
+
+                MessageBox.Show("导入完成");
+
+                return workbook;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+                return null;
+            }
+        }
+
+        private string[] resultTableColHeader = new string[]
+        {
+            "员工编号",
+            "员工姓名",
+            "1月份",
+            "2月份",
+            "3月份",
+            "4月份",
+            "5月份",
+            "6月份",
+            "7月份",
+            "8月份",
+            "9月份",
+            "10月份",
+            "11月份",
+            "12月份",
+            "合计"
+        };
+        
+        private void Save(Workbook workbook)
+        {
+            try
+            {
+                Worksheet sheet = workbook.Worksheets[workbook.Worksheets.Count - 1];
+                if (sheet.Name != "工资汇总(自动生成)")
+                {
+                    sheet = workbook.CreateEmptySheet("工资汇总(自动生成)");
+                }
+
+                CellStyle cellStyle = sheet.GetDefaultRowStyle(1);
+                cellStyle.VerticalAlignment = VerticalAlignType.Center;
+                cellStyle.HorizontalAlignment = HorizontalAlignType.Center;
+
+                for (int c = 1; c <= 15; c++)
+                {
+                    string cName = ColIndexToText(c);
+                    sheet.Range[string.Format(cName + "{0}:" + cName + "{1}", 1, 3)].Merge();
+                    sheet.Range[string.Format(cName + "{0}:" + cName + "{1}", 1, 3)].Text = resultTableColHeader[c - 1];
+
+                }
+
+                int row = 4;
+                int g1EndRow = row = WriteGroupEmployee(sheet, listGroup1Employes, row, "银行发放小计");
+                int g2EndRow = row = WriteGroupEmployee(sheet, listGroup2Employes, row + 1, "现金发放小计");
+
+                row++;
+                sheet.Range[string.Format("A{0}:B{0}", row)].Merge();
+                sheet.Range[string.Format("A{0}:B{0}", row)].Text = "员工工资合计";
+
+                for (int c = 0; c < 13; c++)
+                {
+                    string cName = ColIndexToText(c + 3);
+                    sheet.Range[string.Format(cName + "{0}", row)].Value2 = string.Format("=SUM(" + cName + "{0}+" + cName + "{1})", g1EndRow, g2EndRow);
+                }
+
+                int g3EndRow = row = WriteGroupEmployee(sheet, listGroup3Employes, row + 1, "外来人员小计");
+                row++;
+                sheet.Range[string.Format("A{0}:B{0}", row)].Merge();
+                sheet.Range[string.Format("A{0}:B{0}", row)].Text = "外账工资报表合计";
+
+                for (int c = 0; c < 13; c++)
+                {
+                    string cName = ColIndexToText(c + 3);
+                    sheet.Range[string.Format(cName + "{0}", row)].Value2 = string.Format("=SUM(" + cName + "{0}+" + cName + "{1})", g1EndRow, g3EndRow);
+                }
+
+
+                workbook.Save();
+                MessageBox.Show("汇总成功");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("汇总失败。" + exc.Message);
+            }
+        }
+
+        private int WriteGroupEmployee(Worksheet sheet, List<Employee> list, int row, string sumTitle)
+        {
+            int startRow = row;
+            for (int i = 0; i < list.Count; i++)
+            {
+                sheet.Range[string.Format("A{0}", row)].Value = (i + 1).ToString();
+                sheet.Range[string.Format("B{0}", row)].Text = list[i].Name;
+
+                for (int c = 0; c < 12; c++)
+                {
+                    string cName = ColIndexToText(c + 3);
+                    sheet.Range[string.Format(cName + "{0}", row)].Value = list[i].Salaries[c].ToString();
+                }
+                sheet.Range[string.Format("O{0}", row)].Value2 = string.Format("=SUM(C{0}:N{0})", row);
+
+                row++;
+            }
+
+
+            sheet.Range[string.Format("A{0}:B{0}", row)].Merge();
+            sheet.Range[string.Format("A{0}:B{0}", row)].Text = sumTitle;
+
+            for (int c = 0; c < 13; c++)
+            {
+                string cName = ColIndexToText(c + 3);
+                sheet.Range[string.Format(cName + "{0}", row)].Value2 = string.Format("=SUM(" + cName + "{0}:" + cName + "{1})", startRow, row - 1);
+            }
+
+            return row;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="colIndex">from 1</param>
+        /// <returns></returns>
+        private string ColIndexToText(int colIndex)
+        {
+            if (colIndex == 1)
+            {
+                return "A";
+            }
+            if (colIndex == 2)
+            {
+                return "B";
+            }
+            if (colIndex == 3)
+            {
+                return "C";
+            }
+            if (colIndex == 4)
+            {
+                return "D";
+            }
+            if (colIndex == 5)
+            {
+                return "E";
+            }
+            if (colIndex == 6)
+            {
+                return "F";
+            }
+            if (colIndex == 7)
+            {
+                return "G";
+            }
+            if (colIndex == 8)
+            {
+                return "H";
+            }
+            if (colIndex == 9)
+            {
+                return "I";
+            }
+            if (colIndex == 10)
+            {
+                return "J";
+            }
+            if (colIndex == 11)
+            {
+                return "K";
+            }
+            if (colIndex == 12)
+            {
+                return "L";
+            }
+            if (colIndex == 13)
+            {
+                return "M";
+            }
+            if (colIndex == 14)
+            {
+                return "N";
+            }
+            if (colIndex == 15)
+            {
+                return "O";
+            }
+            if (colIndex == 16)
+            {
+                return "P";
+            }
+            if (colIndex == 17)
+            {
+                return "Q";
+            }
+            if (colIndex == 18)
+            {
+                return "R";
+            }
+            if (colIndex == 19)
+            {
+                return "S";
+            }
+            if (colIndex == 20)
+            {
+                return "T";
+            }
+            if (colIndex == 21)
+            {
+                return "U";
+            }
+            return "Z";
+        }
+
+        private int ReadGroupEmployee(Worksheet sheet, int startRow, List<Employee> list, int month)
+        {
+            int r = startRow;
+            for (; r <= sheet.LastRow; r++)
+            {
+                string name = sheet[r, this.cmdEmplyeeNameCol.SelectedIndex + 1].Value.Trim();
+                if (string.IsNullOrEmpty(name))
+                {
+                    break;
+                }
+                int salary = 0;
+                try
+                {
+                    object txtSalary = sheet[r, this.cmbSalaryCol.SelectedIndex + 1].FormulaValue;
+                    salary = Convert.ToInt32(txtSalary);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("表［" + sheet.Name + "]工资列 第" + r.ToString() + "行非数值，汇总失败请检查！");
+                    break;
+                }
+
+                Employee emp = new Employee();
+
+                emp = list.FirstOrDefault(e => e.Name == name);
+                if (emp == null)
+                {
+                    emp = new Employee()
+                    {
+                        Name = name
+                    };
+                    list.Add(emp);
+                }
+                emp.Salaries[month - 1] = salary;
+
+
+                Console.WriteLine(sheet[r, 1].Value.Trim());
+            }
+
+            return r;
+        }
+
+        private void btnLookUp_Click(object sender, EventArgs e)
+        {
+            this.listGroup1Employes.Clear();
+            this.listGroup2Employes.Clear();
+            this.listGroup3Employes.Clear();
+
+            Workbook workbook = this.LoadFile(this.filePath);
+            if (workbook != null)
+            {
+                this.Save(workbook);
+            }
+        }
+    }
+
+    public class Employee
+    {
+        public string Name;
+        public int[] Salaries = new int[12];
+    }
+}
